2c27af6a28ae61ffdfdc666bc96c3c5ecd570bfd
hpodl <hubert.podlewski.stud@pw.edu.pl>
2023-08-05 00:56:40 +0200

    Added parse method to Response

--- src/response.rs
+++ src/response.rs
@@ -1,9 +1,15 @@
-use serde::{Deserialize, Serialize};
 use crate::message::MessageProto;
+use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
 pub enum Response {
     Messages(Vec<MessageProto>), // currently there's no simple way to serialize slices of arbitrary size
     MessageAdded(),
     Invalid,
+}
+
+impl Response {
+    pub fn parse<'a>(response: &'a [u8]) -> Response {
+        serde_json::from_slice::<Response>(response).unwrap_or(Response::Invalid)
+    }
 }
