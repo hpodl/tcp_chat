2876100b13bda3d7e4a9031d0490d88a13523f2e
hpodl <hubert.podlewski.stud@pw.edu.pl>
2023-08-05 01:36:18 +0200

    Added Display impl for Message, moved Debug derive

--- src/message.rs
+++ src/message.rs
@@ -1,10 +1,18 @@
+use std::fmt::Display;
+
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize)]
-#[cfg_attr(test, derive(Debug, PartialEq))]
+#[derive(Serialize, Deserialize, Debug)]
+#[cfg_attr(test, derive(PartialEq))]
 pub struct Message {
     pub content: MessageProto,
     pub id: usize,
+}
+
+impl Display for Message {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_fmt(format_args!("[{}]{}: {}", self.id, self.content.user, self.content.content))
+    }
 }
 
 impl Message {
@@ -13,8 +21,8 @@ impl Message {
     }
 }
 
-#[derive(Serialize, Deserialize)]
-#[cfg_attr(test, derive(Debug, PartialEq))]
+#[derive(Serialize, Deserialize, Debug)]
+#[cfg_attr(test, derive(PartialEq))]
 pub struct MessageProto {
     pub content: String,
     pub user: String,
